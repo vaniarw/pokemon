7e8a7a79c6407123eec764367dbda8be6f74d883
Vania Walangare <vania.raina9@gmail.com>
2019-06-13 13:52:30 -0700

    Styled the Page

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello World!"/>
-    <card :url="item.pokemon.url" v-for="item in pokemonOfRockType" :key="item.pokemon.name"></card>
-
-    <div v-for="item in pokemonOfRockType" :key="item.pokemon.name">
-      {{ item.pokemon.name }} <br>
-      {{ item.pokemon.url }}<br><br>
+    <div class="container">
+      <h1 class="text-center display-1 m-5">Rock Type Pokemon</h1>
+      <div class="row">
+        <card :url="item.pokemon.url" v-for="item in pokemonOfRockType" :key="item.pokemon.name"></card>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import card from './components/card.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import card from "./components/card.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     card
   },
-  data: function ()
-      {
-          return {
-              pokemonOfRockType: ""
-          }
-      },
-      mounted: function() {
-        console.log("mounted function ran")
+  data: function() {
+    return {
+      pokemonOfRockType: ""
+    };
+  },
+  mounted: function() {
+    console.log("mounted function ran");
 
-        const axios = require('axios');
-        const vm = this;
+    const axios = require("axios");
+    const vm = this;
 
-        axios({
-            method: 'get',
-            url: 'https://pokeapi.co/api/v2/type/rock',
-            responseType: 'stream'
-        })
-            .then(function (response) {
-                // console.log(response.data.pokemon);
-                vm.pokemonOfRockType = response.data.pokemon
-                });
-    }
-}
+    axios({
+      method: "get",
+      url: "https://pokeapi.co/api/v2/type/rock",
+      responseType: "stream"
+    }).then(function(response) {
+      // console.log(response.data.pokemon);
+      vm.pokemonOfRockType = response.data.pokemon;
+    });
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/card.vue
+++ src/components/card.vue
@@ -1,65 +1,53 @@
 <template>
-  <div class="hello">
+  <div class="col-3">
     <div class="card" style="width: 18rem;">
-        <img class="card-img-top" v-bind:src="pokemon.sprites['front_default']" alt="None">
-        <!-- <img class="card-img-top" v-bind:src="pokemon.sprites.font_default" alt="None"> -->
-        <div class="card-body">
-            <h1>{{url}}</h1>
-            <h5 class="card-title">{{ pokemon.name }}</h5>
-            <p class="card-text"> Height: {{ pokemon.height }} inches 
-            <br> Weight: {{ pokemon.weight }} pounds</p>
-            
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+      <!-- <img class="card-img-top" v-bind:src="pokemon.sprites.font_default" alt="None"> -->
+      <div class="card-body">
+        <img :src="pokemon.sprites.front_default" alt="None">
+        <h5 class="card-title text-capitalize">{{ pokemon.name }}</h5>
+        <p class="card-text">
+          Height: {{ pokemon.height }} inches
+          <br>
+          Weight: {{ pokemon.weight }} pounds
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'card',
-  data: function ()
-      {
-          return {
-              pokemon: ""
-          }
-      },
+  name: "card",
+  data: function() {
+    return {
+      pokemon: ""
+    };
+  },
   props: {
     url: String
   },
   mounted: function() {
-      console.log("mounted function ran")
+    console.log("mounted function ran");
 
-      const axios = require('axios');
-      const vm = this;
+    const axios = require("axios");
+    const vm = this;
 
-      axios({
-            method: 'get',
-            url: vm.url,
-            responseType: 'stream'
-        })
-            .then(function (response) {
-                // console.log(response);
-                vm.pokemon = response.data
-                });
-    }
-}
+    axios({
+      method: "get",
+      url: vm.url,
+      responseType: "stream"
+    }).then(function(response) {
+      // console.log(response);
+      vm.pokemon = response.data;
+    });
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.card {
+    text-align: center;
+    margin-bottom: 30px;
 }
 </style>
