93965994dbac10c1f57a76a5ef442b55d9fb687d
Vania Walangare <vania.raina9@gmail.com>
2019-06-13 10:34:58 -0700

    First Commit Created Single Pokemon Card

---
+++ src/components/card.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" v-bind:src="pokemon.sprites['front_default']" alt="None">
+        <!-- <img class="card-img-top" v-bind:src="pokemon.sprites.font_default" alt="None"> -->
+        <div class="card-body">
+            <h5 class="card-title">{{ pokemon.name }}</h5>
+            <p class="card-text"> Height: {{ pokemon.height }} inches 
+            <br> Weight: {{ pokemon.weight }} pounds</p>
+            
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  data: function ()
+      {
+          return {
+              pokemon: null
+          }
+      },
+  props: {
+    msg: String
+  },
+  mounted: function() {
+      console.log("mounted function ran")
+
+      const axios = require('axios');
+      const vm = this;
+
+      axios({
+            method: 'get',
+            url: 'https://pokeapi.co/api/v2/pokemon/pikachu',
+            responseType: 'stream'
+        })
+            .then(function (response) {
+                console.log(response);
+                vm.pokemon = response.data
+                });
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
